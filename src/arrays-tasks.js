e7e247bd91e5297599506a9ec4c823ac2ca0a9d0
vladimirovicp <vladimirovicp70@gmail.com>
2024-12-05 11:44:57 +0400

    feat: getMaxItems

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -517,8 +517,13 @@ function getHexRGBValues(arr) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  if (arr.length === 0) {
+    return [];
+  }
+
+  arr.sort((a, b) => b - a);
+  return [...arr.slice(0, n)];
 }
 
 /**
